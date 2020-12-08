d6ce151ec943ca484f4ce39f9b22fc32154b2d0c
arich <arich@scwa.ca.gov>
2020-12-08 08:17:37 -0800

    final updates

--- Run_updated_Model.bat
+++ Run_updated_Model.bat
@@ -1,7 +1,9 @@
 REM
-gsflow_ag.exe SRPHM_update.control
-REM gsflow.exe SRPHM_update.control
+call gsflow_ag.exe SRPHM_update.control
 
+call C:\GSP\srp\ag_py\zone_budget\run_zonebud.bat
+call C:\GSP\srp\ag_py\zone_budget\run_aniparse.bat
+call C:\GSP\srp\ag_py\zone_budget\run_sfrprocess.bat
 ECHO.
 ECHO Run complete. Please press enter when you want to continue.
 PAUSE>NUL
