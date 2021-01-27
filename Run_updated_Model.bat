68559d0f6c7f6327057308424720d0ea386147d2
arich <arich@scwa.ca.gov>
2021-01-26 17:54:13 -0800

    changed .bat file

--- Run_updated_Model.bat
+++ Run_updated_Model.bat
@@ -1,9 +1,9 @@
 REM
-call gsflow_ag.exe SRPHM_update.control
+call gsflow_ag_2021.exe SRPHM_update.control
 
-call C:\GSP\srp\ag_py\zone_budget\run_zonebud.bat
-call C:\GSP\srp\ag_py\zone_budget\run_aniparse.bat
-call C:\GSP\srp\ag_py\zone_budget\run_sfrprocess.bat
+rem call C:\GSP\srp\ag_py\zone_budget\run_zonebud.bat
+rem call C:\GSP\srp\ag_py\zone_budget\run_aniparse.bat
+rem call C:\GSP\srp\ag_py\zone_budget\run_sfrprocess.bat
 ECHO.
 ECHO Run complete. Please press enter when you want to continue.
 PAUSE>NUL
